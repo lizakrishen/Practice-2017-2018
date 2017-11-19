409f03cbdb52b0b6d288d515009e4af9a78f87ff
lizakrishen <miss.krishen80@gmail.com>
2017-11-19 23:59:36 +0200

    lab 1.3

---
+++ C++ Ess/Chapter 1/Lab 1.3/lab 1.3.1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>//we included iostream
+#include <iomanip>//we included iomanip
+#include <string>//we included string
+int main()
+{
+	int v=10800; // 3*60*60
+	int zzz=3*60;// This is a variable to hold the value of 3 minutes in seconds
+	int zzz=5*60;
+	float siii=3.141526; //This is the value of pi
+}
